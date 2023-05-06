665224478c9ef4215841234382d6641138f7c220
FitzFlow <florian.etheve@epitech.eu>
2023-05-07 01:31:57 +0400

    fix text writing for nothing

--- templates/layout/homeLayout.php
+++ templates/layout/homeLayout.php
@@ -1,6 +1,5 @@
 <?= $this->Html->css(['home1.css']) ?>
 <?= $this->fetch('css') ?>
-<?= $this->Url->build('/', ['fullBase' => true]) ?>img/starmap2.jpg
 
 <?= $this->Html->script(['home.js']) ?>
 <?= $this->fetch('script') ?>
